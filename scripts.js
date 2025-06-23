dc55dbb82576f029fcf086684f15b077ac8984a0
Emon Adhikari <102776222+Euuiiii@users.noreply.github.com>
2025-06-23 14:13:15 +0545

    TO DO APP

---
+++ scripts.js
@@ -0,0 +1,50 @@
+const taskInput = document.getElementById("taskInput");
+const addTaskButton = document.getElementById("addTaskButton");
+const taskList = document.getElementById("taskList");
+const clearTasksButton = document.getElementById("clearTasks");
+const taskDueDate = document.getElementById("dueDateInput");
+const taskDueTime = document.getElementById("dueTimeInput");
+const taskTemplate = document.getElementById("taskTemplate");
+const taskDescriptionInput = document.getElementById("taskDiscription");
+
+const addTask = () => {
+    const taskText = taskInput.value.trim();
+    const description = taskDescriptionInput.value.trim();
+    const dueDate = taskDueDate.value;
+    const dueTime = taskDueTime.value;
+    if (taskText === "") {
+        alert("You must add a task mf!");
+        return;
+    }  
+
+    const clone = taskTemplate.content.cloneNode(true);
+    clone.querySelector(".taskText").textContent = taskText;
+    clone.querySelector(".taskDescription").textContent = description;
+    clone.querySelector(".taskDueDate").textContent = "Due: " + dueDate;
+    clone.querySelector(".taskDueTime").textContent = " at " + dueTime;
+
+    const deleteButton = clone.querySelector(".deleteTaskButton");
+    const taskMain = clone.querySelector(".taskMain");
+    const taskDetails = clone.querySelector(".taskDetails");
+
+
+    const listItem = document.createElement("li");
+    listItem.appendChild(clone);
+    taskList.appendChild(listItem);
+
+    deleteButton.onclick = (e) => {
+        e.stopPropagation();
+        listItem.remove();
+    };
+
+    taskMain.onclick = () => {
+        taskDetails.style.display = taskDetails.style.display === "none" ? "block" : "none";
+    };
+
+    taskInput.value = "";
+    taskDescriptionInput.value = "";
+    taskDueDate.value = "";
+    taskDueTime.value = "";
+}
+
+addTaskButton.addEventListener("click", addTask);
